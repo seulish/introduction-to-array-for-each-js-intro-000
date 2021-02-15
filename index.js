873918569f590f50bf023aed678c0cc0e903de3c
Leah Lee <leah@iterable.com>
2021-02-15 00:05:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ function doToElementsInArray(array, callback) {
   array.forEach(callback);
 }
 // Add your changeCompletely() function here:
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
